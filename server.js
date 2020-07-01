c1b06043bc57150b6db1600623052a50b540a54a
Tedros <tedtfu@gmail.com>
2020-07-01 19:56:13 -0400

    test

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.post("/api/login",async (req,res)=>{
 res.send(await login(req.body));
     });
 
-
-app.listen(process.env.PORT,"0.0.0.0",()=>console.log(`web service running on port ${PORT}`));
+    console.log(process.env.PORT)
+app.listen(process.env.PORT,"0.0.0.0",()=>console.log(`web service running on port ${process.env.PORT}`));
